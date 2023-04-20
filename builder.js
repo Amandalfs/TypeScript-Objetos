b7906a49af1638e77f6d11402640791b768c44f2
Amandalfs <caduc57@gmail.com>
2023-04-19 22:37:03 -0300

    feat
    criado as pastas e arquivos

---
+++ builder.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+
+const numberOfFolders = 10; // número de pastas a serem criadas
+
+for(let i = 1; i <= numberOfFolders; i++) {
+
+    const folderName = `0${i}`; // nome da pasta a ser criada
+
+    const fileName = `ex${i}.ts`
+    const fileContent = "//Escreve seu codigo aqui"
+    fs.mkdir(folderName, (err) => { // cria a pasta usando o método mkdir
+        if (err) {
+            console.error(err);
+        } else {
+            console.log(`Pasta ${folderName} criada com sucesso!`);
+            fs.writeFile(`${folderName}/${fileName}`, fileContent, (err) => { // cria o arquivo dentro da pasta
+                if (err) {
+                    console.error(err);
+                } else {
+                    console.log(`Arquivo ${fileName} criado dentro da pasta ${folderName} com sucesso!`);
+                }
+      });
+      
+    }
+  });
+}
