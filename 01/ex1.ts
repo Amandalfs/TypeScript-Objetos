c4800eeeb398510f2ca92abe27489df432ddf547
Amandalfs <caduc57@gmail.com>
2023-04-20 17:49:46 -0300

    Exercicio 01 Concluido

--- 01/ex1.ts
+++ 01/ex1.ts
@@ -1,1 +1,15 @@
 //Escreve seu codigo aqui
+
+interface Usuarios {
+    nome: string,
+    email: string, 
+    senha: string
+}
+
+let user1: Usuarios = {
+    nome: "Amanda",
+    email: "amanda@gmail.com",
+    senha:"1253458485"
+} 
+
+console.log(user1)
