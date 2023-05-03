2fc59a1b6eec19e0bedefe230eb042fc829857c0
Amandalfs <caduc57@gmail.com>
2023-05-03 12:02:15 -0300

    Exercicio 09 Concluido

--- 07/ex7.ts
+++ 07/ex7.ts
@@ -1,2 +1,15 @@
 //Escreve seu codigo aqui
 
+interface Produto {
+    nome: string,
+    descricao: string,
+    preco: number,   
+}
+
+const pcGamer: Produto = {
+    nome: "razer 5",
+    descricao: "melhor pc gamer",
+    preco: 5000
+}
+
+console.log(pcGamer)
